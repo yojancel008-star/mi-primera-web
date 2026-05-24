3b653f8c047be538cb1cca523c18fdcb0d02862f
yojancel008-star <yojancel008@gmail.com>
2026-05-24 16:22:49 -0400

    kkkk

--- script.js
+++ script.js
@@ -42,3 +42,45 @@ function modoOscuro() {
   document.body.classList.toggle("oscuro");
 }
 
+
+
+
+
+
+
+const bmw = document.querySelectorAll(".bmw");
+const honda = document.querySelectorAll(".honda");
+
+
+function mostrarTodos() {
+  bmw.forEach(carro => carro.style.display = "block");
+  honda.forEach(carro => carro.style.display = "block");
+}
+
+function mostrarBMW() {
+  bmw.forEach(carro => carro.style.display = "block");
+  honda.forEach(carro => carro.style.display = "none");
+}
+
+function mostrarHonda() {
+  honda.forEach(carro => carro.style.display = "block");
+  bmw.forEach(carro => carro.style.display = "none");
+}
+
+function mostrarMercedes() {
+  bmw.forEach(carro => carro.style.display = "none");
+  honda.forEach(carro => carro.style.display = "none");
+}
+
+
+function abrirModal(src) {
+
+  document.getElementById("modal").style.display = "flex";
+
+  document.getElementById("imagenGrande").src = src;
+}
+
+function cerrarModal() {
+
+  document.getElementById("modal").style.display = "none";
+}
